cca5487a023bac1b50284e24b0c975a55fddf99e
Adron Hall <adron@Adrons-MacBook-Pro.local>
2016-04-09 14:38:08 -0700

    Combined install scripts.

---
+++ scripts/install-terraform-and-packer.sh
@@ -0,0 +1,22 @@
+cd /usr/local/
+mkdir terraform_0_6_14
+mkdir packer_0_10_0
+cd terraform_0_6_14
+curl -O https://releases.hashicorp.com/terraform/0.6.14/terraform_0.6.14_darwin_amd64.zip
+unzip terraform_0.6.14_darwin_amd64.zip
+
+cd /usr/local/packer_0_10_0
+curl -O https://releases.hashicorp.com/packer/0.10.0/packer_0.10.0_darwin_amd64.zip
+unzip packer_0.10.0_darwin_amd64.zip
+
+cat <<EOT >> ~/.bash_profile
+
+# Terraform & Packer Paths.
+export PATH=/usr/local/packer_0_10_0/:/usr/local/terraform_0_6_14/:$PATH
+
+EOT
+
+source ~/.bash_profile
+
+terraform
+packer
