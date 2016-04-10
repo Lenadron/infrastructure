e71dc1befc7119ab104034d3bece354a678b9757
Adron Hall <adron@Adrons-MacBook-Pro.local>
2016-04-09 21:20:50 -0700

    setup script.

--- setup-environment.sh
+++ setup-environment.sh
@@ -5,4 +5,4 @@ docker-machine start $MACHINE
 docker-machine env $MACHINE
 eval $(docker-machine env $MACHINE)
 docker pull jenkins
-docker run -p 8080:8080 -p 50000:50000 -d jenkins
+docker run -p 8080:8080 -p 50000:50000 -v ~/Codez/infrastructure/jenkins:/var/jenkins_home -d jenkins
