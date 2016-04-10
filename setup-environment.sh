399d47990a1803774bae8edd7493f68aa9342ef4
Adron Hall <adron@Adrons-MacBook-Pro.local>
2016-04-09 21:28:24 -0700

    That.

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,8 +1,8 @@
-MACHINE="default"
+MACHINE="microservices"
 
 docker-machine create $MACHINE --driver=virtualbox
 docker-machine start $MACHINE
 docker-machine env $MACHINE
 eval $(docker-machine env $MACHINE)
 docker pull jenkins
-docker run -p 8080:8080 -p 50000:50000 -v ~/Codez/infrastructure/jenkins:/var/jenkins_home -d jenkins
+docker run -p 8080:8080 -p 50000:50000 -v /jenkins:/var/jenkins_home -d jenkins
