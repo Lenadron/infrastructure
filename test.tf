9d657802575046038ec9ef7e301ed02b7e8df338
Adron Hall <adron@Adrons-MacBook-Pro.local>
2016-04-10 22:51:27 -0700

    Blargh.

--- test.tf
+++ test.tf
@@ -1,11 +1,11 @@
 # Start a container
-resource "docker_container" "ubuntu" {
-  name = "foo"
-  image = "${docker_image.ubuntu.latest}"
-  command = ["echo"]
+resource "docker_container" "jenkins" {
+  name = "jenkins_tf_server"
+  image = "${docker_image.jenkins.latest}"
+  command = ["docker run -p 8080:8080 -p 50000:50000 -v /jenkins:/var/jenkins_home -d jenkins"]
 }
 
 # Find the latest Ubuntu precise image.
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:precise"
+resource "docker_image" "jenkins" {
+  name = "jenkins"
 }
